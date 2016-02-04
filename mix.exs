27c381e20ccef72a5d4951d545661e1e9d9d27a7
Brian Cardarella <brian@dockyard.com>
2016-02-04 03:01:35 -0500

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Inquisitor.Mixfile do
 
   def project do
     [app: :inquisitor,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
