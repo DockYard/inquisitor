c0da51203b990930a0974815a0851fea915d789a
Brian Cardarella <brian@dockyard.com>
2016-02-04 02:45:13 -0500

    Code refactor
    Clean up

--- lib/inquisitor.ex
+++ lib/inquisitor.ex
@@ -8,46 +8,34 @@ defmodule Inquisitor do
   end
 
   defmacro __before_compile__(env) do
-    model = Module.get_attribute(env.module, :__inquisitor__model__)
+    model     = Module.get_attribute(env.module, :__inquisitor__model__)
     whitelist = Module.get_attribute(env.module, :__inquisitor__whitelist__)
-    name =
-      model
-      |> Code.eval_quoted()
-      |> elem(0)
-      |> inspect()
-      |> String.split(".")
-      |> List.last
-      |> Mix.Utils.underscore()
-
-    name =
-      "build_#{name}_query"
-      |> String.to_atom
+    fn_name   = :"build_#{name_from_model(model)}_query"
 
     quote do
-      def unquote(name)(params) do
-        query = unquote(model)
+      def unquote(fn_name)(params) do
         list =
           params
           |> Map.to_list()
           |> Inquisitor.whitelist_filter(unquote(whitelist))
           |> Inquisitor.preprocess()
-        unquote(name)(query, list)
+        unquote(fn_name)(unquote(model), list)
       end
 
-      def unquote(name)(query, []), do: query
-      def unquote(name)(query, [{attr, value}|tail]) do
+      def unquote(fn_name)(query, []), do: query
+      def unquote(fn_name)(query, [{attr, value}|tail]) do
         query
         |> where([r], field(r, ^String.to_existing_atom(attr)) == ^value)
-        |> unquote(name)(tail)
+        |> unquote(fn_name)(tail)
       end
-
     end
   end
 
   def preprocess([]), do: []
   def preprocess([{attr, value}|tail]) when value == "true" or value == "false" do
-    value = Ecto.Type.cast(:boolean, value) |> elem(1)
-    preprocess([{attr, value} | tail])
+    Ecto.Type.cast(:boolean, value)
+    |> elem(1)
+    |> (&preprocess([{attr, &1} | tail])).()
   end
   def preprocess([{attr, value}|tail]),
     do: [{attr, value} | preprocess(tail)]
@@ -55,5 +43,15 @@ defmodule Inquisitor do
   def whitelist_filter(params, nil), do: params
   def whitelist_filter(params, whitelist) do
     Enum.filter(params, &Enum.member?(whitelist, elem(&1, 0)))
+  end
+
+  defp name_from_model(model) do
+    model
+    |> Code.eval_quoted()
+    |> elem(0)
+    |> inspect()
+    |> String.split(".")
+    |> List.last()
+    |> Mix.Utils.underscore()
   end
 end
