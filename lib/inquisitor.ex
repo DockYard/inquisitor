5202f04dae4c28e9b8e2af9f43da187d34162cd1
Brian Cardarella <brian@dockyard.com>
2017-03-25 15:52:15 -0400

    context instead of conn

--- lib/inquisitor.ex
+++ lib/inquisitor.ex
@@ -19,33 +19,33 @@ defmodule Inquisitor do
   `[GET] /posts?foo=hello&bar=world` may result in a params map of
   `%{"foo" => "hello", "bar" => "world"}`
 
-      def index(conn, params) do
+      def index(context, params) do
         posts =
           Post
-          |> build_query(conn, params)
+          |> build_query(context, params)
           |> Repo.all()
 
-        json(conn, posts)
+        json(context, posts)
       end
 
   ## Writing custom query handlers
 
   Custom query handlers are written using `build_query/4`. They accumulatd
   query is the first argument. The key/value pairs
-  extracted from the params become the 2nd and 3rd argument. The `conn` is the last argument.
+  extracted from the params become the 2nd and 3rd argument. The `context` is the last argument.
 
   The return value of the function *must* be a query.
 
-      def build_query(query, "title", title, _conn) do
+      def build_query(query, "title", title, _context) do
         Ecto.Query.where(query, [r], r.title == ^title)
       end
 
   """
   defmacro __using__(_opts) do
     quote do
-      def build_query(query, %Plug.Conn{} = conn, params) do
+      def build_query(query, context, params) do
         Enum.reduce(params, query, fn({key, value}, query) ->
-          build_query(query, key, value, conn)
+          build_query(query, key, value, context)
         end)
       end
 
@@ -55,7 +55,7 @@ defmodule Inquisitor do
 
   defmacro __before_compile__(_env) do
     quote do
-      def build_query(query, _key, _value, %Plug.Conn{} = _conn), do: query
+      def build_query(query, _key, _value, _context), do: query
       defoverridable [build_query: 4]
     end
   end
--- test/inquisitor_test.exs
+++ test/inquisitor_test.exs
@@ -1,25 +1,25 @@
 defmodule InquisitorTest do
   use ExUnit.Case
 
-  @conn %Plug.Conn{}
+  @context %{}
 
   defmodule Basic do
     require Ecto.Query
     use Inquisitor
 
-    def build_query(query, "order_by", field, _conn) do
+    def build_query(query, "order_by", field, _context) do
       Ecto.Query.order_by(query, [asc: ^String.to_existing_atom(field)])
     end
 
-    def build_query(query, "name", name, _conn) do
+    def build_query(query, "name", name, _context) do
       Ecto.Query.where(query, [r], r.name == ^name)
     end
 
-    def build_query(query, "age", age, _conn) when age == "20" do
+    def build_query(query, "age", age, _context) when age == "20" do
       Ecto.Query.where(query, [r], r.age == 44)
     end
 
-    def build_query(query, "age", age, _conn) do
+    def build_query(query, "age", age, _context) do
       Ecto.Query.where(query, [r], r.age == ^age)
     end
   end
@@ -29,22 +29,22 @@ defmodule InquisitorTest do
   end
 
   test "will build a simple key/value query" do
-    q = Basic.build_query(User, @conn, %{"name" => "Brian"})
+    q = Basic.build_query(User, @context, %{"name" => "Brian"})
     assert to_sql(q) == {~s{SELECT u0."id", u0."name", u0."age" FROM "users" AS u0 WHERE (u0."name" = $1)}, ["Brian"]}
   end
 
   test "will combine multiple params with AND sql" do
-    q = Basic.build_query(User, @conn, %{"name" => "Brian", "age" => 36})
+    q = Basic.build_query(User, @context, %{"name" => "Brian", "age" => 36})
     assert to_sql(q) == {~s{SELECT u0."id", u0."name", u0."age" FROM "users" AS u0 WHERE (u0."age" = $1) AND (u0."name" = $2)}, [36, "Brian"]}
   end
 
   test "can use custom composable query" do
-    q = Basic.build_query(User, @conn, %{"order_by" => "name"})
+    q = Basic.build_query(User, @context, %{"order_by" => "name"})
     assert to_sql(q) == {~s{SELECT u0."id", u0."name", u0."age" FROM "users" AS u0 ORDER BY u0."name"}, []}
   end
 
   test "def build_query(query, can take guards" do
-    q = Basic.build_query(User, @conn, %{"age" => "20"})
+    q = Basic.build_query(User, @context, %{"age" => "20"})
     assert to_sql(q) == {~s{SELECT u0."id", u0."name", u0."age" FROM "users" AS u0 WHERE (u0."age" = 44)}, []}
   end
 end
